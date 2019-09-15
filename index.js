3021972995f425b15e1bc1f62b16f6867ec5cc37
Tanner Patterson <tpatterson5492@gmail.com>
2019-09-15 17:58:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logShout(string) {
   }
   
   function sayHiToGrandma(string) {
-      if(string == ''.toLowerCase){
+      if(string === ){
        return string} 
             else {
          return string}
