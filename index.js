248e2a9ee46ab180b6a6f87d88262a71643ddc49
Tanner Patterson <tpatterson5492@gmail.com>
2019-09-15 18:41:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function logShout(string) {
       if(string.toUpperCase() === string){
        return "YES INDEED!"}
          
-         else { if(string = "I loe you, Grandma."){
-           return ""
-         }
+         else {
+      
+      if(string.toUpperCase() == string){
+           return "I love you, too."}
+      }
   }
